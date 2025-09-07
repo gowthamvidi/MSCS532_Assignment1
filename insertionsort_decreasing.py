f576e36967b18d4e2ec3a98ef215ca347b8e53ca
Shiva Gowtham Kumar Vidiyala <svidiyala40297@ucumberlands.edu>
2025-09-07 17:26:19 -0500

    logic added for insertion sort

--- insertionsort_decreasing.py
+++ insertionsort_decreasing.py
@@ -1,3 +1,13 @@
 
 # Initializing Array
-random_array = [19, 99, 53, 25, 46]  
+random_array = [19, 99, 53, 25, 46] 
+
+# logic to sort array
+for i in range(1, len(random_array)):
+        temp = random_array[i]
+        j = i - 1
+        while j >= 0 and random_array[j] < temp:
+            random_array[j + 1] = random_array[j]
+            j = j - 1
+
+        random_array[j + 1] = temp    
